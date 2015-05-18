78ef1755a053538c417cf7fa4317e0ef33a4dd20
Markus Olsen <molsen13@gmail.com>
2015-05-18 13:23:51 -0600

    Idea exists

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150518191712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ideas", force: :cascade do |t|
+    t.text    "title"
+    t.text    "body"
+    t.integer "quality", default: 0
+  end
 
 end
