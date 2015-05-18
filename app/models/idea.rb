5c9206f04ce17da46c4d125295be4d2a13147e0f
Markus Olsen <molsen13@gmail.com>
2015-05-18 14:10:03 -0600

    Incrementing an idea's quality

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,8 @@ class Idea < ActiveRecord::Base
   validates :title, :body, presence: true
 
   enum quality: %w(swill plausible genius)
+
+  def upvote
+    increment!(:quality) if Idea.qualities[self.quality] < 2
+  end
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,21 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Idea" do
-  it "exists with a default quality of swill" do
-    idea = Idea.new
-
-    expect(idea.quality).to eq('swill')
-  end
-
   it "is invalid without title and body" do
     idea = Idea.create(title: nil, body: nil)
+    idea_1 = Idea.create(title: 'hey', body: nil)
+    idea_2 = Idea.create(title: nil, body: 'hey')
 
     expect(idea).to_not be_valid
+    expect(idea_1).to_not be_valid
+    expect(idea_2).to_not be_valid
   end
 
   it "is valid with title and body" do
     idea = Idea.create(title: 'test', body: 'so fun')
 
     expect(idea).to be_valid
+  end
+
+  it "exists with a default quality of swill" do
+    idea = Idea.new
+
+    expect(idea.quality).to eq('swill')
+  end
+
+  it "can have its quality incremented" do
+    idea = Idea.create(title: "test", body: "so fun")
+    idea.upvote
+
+    expect(idea.quality).to eq("plausible")
   end
 end
