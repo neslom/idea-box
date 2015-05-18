d632b4f3e8def1c9c019b93b0ddf23b62bb791ca
Markus Olsen <molsen13@gmail.com>
2015-05-18 13:30:28 -0600

    Require presence of title and body

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,5 @@
 class Idea < ActiveRecord::Base
+  validates :title, :body, presence: true
+
   enum quality: %w(swill plausible genius)
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Idea" do
-  it "exists with a deafult quality of swill" do
+  it "exists with a default quality of swill" do
     idea = Idea.new
 
     expect(idea.quality).to eq('swill')
+  end
+
+  it "is invalid without title and body" do
+    idea = Idea.create(title: nil, body: nil)
+
+    expect(idea).to_not be_valid
   end
 end
